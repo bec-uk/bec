877344901576af3131c39a5315ecd9415622c78f
Tim Barker <tim@spiraledge.co.uk>
2018-05-01 16:56:25 +0100

    updated headline controlled to handle multi sites

--- src/app/controllers/HeadlineController.js
+++ src/app/controllers/HeadlineController.js
@@ -3,20 +3,42 @@
   angular
     .module('app')
     .controller('HeadlineController', [
-      'dataService', 'quantitiesService',
+      'dataService', 'quantitiesService', '$state',
       HeadlineController
     ]);
 
-  function HeadlineController(dataService, quantitiesService) {  
+  function HeadlineController(dataService, quantitiesService, $state) {
 
     self = this;
 
     self.meta = dataService.getMeta;
-    self.data = dataService.getData;
+    // self.data = dataService.getData;
+
+    // self.data = chartSeries;
     self.originalData = dataService.getOriginalData;
     self.factors = dataService.getFactors;
 
     self.quantities = quantitiesService;
+
+    self.getData = function() {
+      let allData = dataService.getData();
+      let siteShortcode = $state.params.shortcode;
+      if(allData.hasOwnProperty(siteShortcode)) {
+          return allData[siteShortcode];
+      } else {
+          return [];
+      }
+    }
+
+    self.getOriginalData = function() {
+      let allData = dataService.getOriginalData();
+      let siteShortcode = $state.params.shortcode;
+      if(allData.hasOwnProperty(siteShortcode)) {
+          return allData[siteShortcode];
+      } else {
+          return [];
+      }
+    }
 
   }
 
