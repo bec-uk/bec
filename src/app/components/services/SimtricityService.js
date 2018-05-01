f9c872d3f72a4b878785a40cba9c38d360f2dbd5
Tim Barker <tim@spiraledge.co.uk>
2018-05-01 15:26:50 +0100

    sorted caching to tie it to the resolution

--- src/app/components/services/SimtricityService.js
+++ src/app/components/services/SimtricityService.js
@@ -20,9 +20,17 @@
 
         function retrieve(params) {
 
-            let cacheKey = (params.siteShortCode + params.exportStartDate + params.exportEndDate + params.resolution).replace(/[^A-Z0-9]/ig, "_");
+            let startDateString = _roundDateToResolution(params.exportStartDate, params.resolution);
+            let endDateString = _roundDateToResolution(params.exportEndDate, params.resolution);
+
+            console.log(startDateString, endDateString);
+
+            let cacheKey = (params.siteShortCode + startDateString + endDateString + params.resolution).replace(/[^A-Z0-9]/ig, "_");
+            // let cacheKey = (params.siteShortCode + params.exportStartDate + params.exportEndDate + params.resolution).replace(/[^A-Z0-9]/ig, "_");
 
             let cacheResult = _checkCache(cacheKey);
+
+            console.log(cacheResult);
 
             if(cacheResult.hit) {
               return $q.resolve({
@@ -76,11 +84,40 @@
             payload: payload,
             expiry: expiry
           }
+          console.log(cacheKey);
+          console.log(cacheItem);
           storage.setItem(cacheKey, JSON.stringify(cacheItem));
+          // storage.setItem("hello", "goodbye");
 
         }
 
+        function _roundDateToResolution(dateString, resolution) {
 
+          let dateMoment = moment(dateString);
+          switch(resolution) {
+            case 'P1D':
+            case "P1W":
+              return dateMoment.format("YYYYMMDD");
+              break;
+            case "PT30M":
+              return dateMoment.format("YYYYMMDDHHmm");
+              break;
+            case "PT1H":
+              return dateMoment.format("YYYYMMDDHH");
+              break;
+            case "P1M":
+            case "P3M":
+              return dateMoment.format("YYYYMM");
+              break;
+            case "P1Y":
+              return dateMoment.format("YYYY");
+              break;
+            default:
+              return dateMoment.format();
+              break;
+          }
+
+        }
 
   }
 
