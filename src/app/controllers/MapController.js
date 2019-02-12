80ab71b9e3ff02f5dd0be2894a40da9879bcce89
Tim Barker <tim@spiraledge.co.uk>
2019-02-12 17:36:59 +0000

    added google map API key

--- src/app/controllers/MapController.js
+++ src/app/controllers/MapController.js
@@ -4,7 +4,7 @@
        .module('app')
        .config(function(uiGmapGoogleMapApiProvider) {
             uiGmapGoogleMapApiProvider.configure({
-                //    key: 'your api key',
+                key: 'AIzaSyCGyv5O3qRvRFpfp_WkeOYuAomF-HrKJdk',
                 v: '3.20', //defaults to latest 3.X anyhow
                 libraries: 'weather,geometry,visualization'
             });
